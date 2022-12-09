07fa07213d1463f5e0303f569030e582c2fc7ba6
agnesolin <agnesolin@gmail.com>
2022-12-09 08:54:28 +0100

    some new attendance plot code

--- help_scripts/AI_GeneralAttendance.R
+++ help_scripts/AI_GeneralAttendance.R
@@ -31,28 +31,51 @@ adults_count$h =  format(adults_count$timedate, "%Y-%m-%d %H")
 adults_h = aggregate(object_count ~ h, adults_count, median)
 adults_h$h = as.POSIXct(adults_h$h, format = "%Y-%m-%d %H")
 
-ggplot(data = adults_h, aes(x = hour(h), y = object_count)) +
-  geom_point() +
-  geom_smooth()
-
-
-ggplot(data = adults_h, aes(x = yday(h), y = object_count)) +
-  geom_point() +
-  geom_smooth()
-
-
-ggplot(data = adults_h, aes(x = yday(h), y = object_count)) +
-  geom_point() +
-  geom_smooth() +
-  facet_grid(~year(h))
-
+library(colortools)
+cols = wheel("steelblue", num = 24)
 
 
 
 ggplot(data = adults_h, aes(x = yday(h), y = object_count, group = hour(h), colour = hour(h))) +
-  geom_point() +
-  geom_smooth() +
-  facet_grid(~year(h))
+  #scale_colour_manual(values = cols) +
+  
+  geom_line() +
+  
+  facet_grid(~year(h)) +
+  
+  ylab("Birds present") + xlab("Day of year") +
+  
+  theme_classic() 
+
+# three different temporal scales: seasonal, diurnal, minute to minute
+
+# seasonal
+ggplot(data = adults_h[hour(adults_h$h) == 0,], 
+       aes(x = yday(h), y = object_count, group = as.factor(year(h)), colour = as.factor(year(h)))) +
+  geom_line() +
+  theme_classic()
+
+
+
+# diel
+ggplot(data = adults_h[year(adults_h$h)== 2021 & yday(adults_h$h) %in%  c(120, 150, 180, 210),], # one early season, one peak breeding
+       aes(x = hour(h), y = object_count, as.factor(group = yday(h)), colour = as.factor(yday(h)))) +
+  geom_line() +
+  ylab("Birds present") + xlab("Hour") +
+  theme_classic()
+
+
+# minute to minute
+adults_count$min =  format(adults_count$timedate, "%Y-%m-%d %H-%M")
+adults_min = aggregate(object_count ~ min, adults_count[year(adults_count$timedate) == 2020,], median)
+adults_min$min = as.POSIXct(adults_min$min, format = "%Y-%m-%d %H-%M")
+
+ggplot(data = adults_min[yday(adults_min$min) %in% c(148) & hour(adults_min$min) %in% c(14) ,], # one with eagle disturbance, one without 
+       aes(x = minute(min), y = object_count, group = yday(min), colour = yday(min))) +
+  geom_line() +
+  #xlim() + # restrict to 1-2 h
+  
+  theme_classic()
 
 # colour scale
 # remove CI
